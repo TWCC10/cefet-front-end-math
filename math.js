5c6f89872e16ce0a68392f373bd8ab71147a98da
Flavio Coutinho <fegemo@gmail.com>
2021-12-16 19:37:41 -0300

    Exercise 3: calculate delta

--- math.js
+++ math.js
@@ -1,7 +1,16 @@
 let resolverEl = document.querySelector('#resolver');
+let aEl = document.querySelector('#coeficiente-a');
+let bEl = document.querySelector('#coeficiente-b');
+let cEl = document.querySelector('#coeficiente-c');
+let deltaEl = document.querySelector('#resultado-delta');
 
 resolverEl.addEventListener('click', resolver);
 
 function resolver() {
-  window.alert('Testando!!');
+  const a = aEl.value;
+  const b = bEl.value;
+  const c = cEl.value;
+  const delta = b ** 2 - 4 * a * c;
+
+  deltaEl.value = delta;
 }
