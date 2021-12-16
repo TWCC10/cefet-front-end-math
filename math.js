d454158cd5776f229369c0727f498beecddc04d3
Flavio Coutinho <fegemo@gmail.com>
2021-12-16 19:49:55 -0300

    Optional 2: more information

--- math.js
+++ math.js
@@ -3,8 +3,11 @@ let aEl = document.querySelector('#coeficiente-a');
 let bEl = document.querySelector('#coeficiente-b');
 let cEl = document.querySelector('#coeficiente-c');
 let deltaEl = document.querySelector('#resultado-delta');
-let x1El = document.querySelector('#resultado-x1')
-let x2El = document.querySelector('#resultado-x2')
+let x1El = document.querySelector('#resultado-x1');
+let x2El = document.querySelector('#resultado-x2');
+let numeroDeRaizesEl = document.querySelector('#resultado-numero-de-raizes');
+let viradaParaEl = document.querySelector('#resultado-virada-para');
+let verticeEl = document.querySelector('#resultado-vertice');
 
 resolverEl.addEventListener('click', resolver);
 
@@ -24,11 +27,25 @@ function resolver() {
 
   let x1 = null;
   let x2 = null;
+  let numeroDeRaizes = null;
+
   if (delta >= 0) {
     x1 = (-b - Math.sqrt(delta)) / (2*a);
     x2 = (-b + Math.sqrt(delta)) / (2*a);
+    numeroDeRaizes = delta === 0 ? 1 : 2;
+
+  } else {
+    numeroDeRaizes = 0;
   }
+
+  let viradaPara = a > 0 ? 'baixo' : 'cima';
+
+  let xVertice = -b / (2*a);
+  let yVertice = -delta / (4*a);
 
   x1El.value = x1;
   x2El.value = x2;
+  numeroDeRaizesEl.value = numeroDeRaizes;
+  viradaParaEl.value = viradaPara;
+  verticeEl.value = '(' + xVertice + ', ' + yVertice + ')';
 }
