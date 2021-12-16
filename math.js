525568cba521d7a737f836a9c502399147a24258
Flavio Coutinho <fegemo@gmail.com>
2021-12-16 19:40:39 -0300

    Exercise 4: full equation

--- math.js
+++ math.js
@@ -3,6 +3,8 @@ let aEl = document.querySelector('#coeficiente-a');
 let bEl = document.querySelector('#coeficiente-b');
 let cEl = document.querySelector('#coeficiente-c');
 let deltaEl = document.querySelector('#resultado-delta');
+let x1El = document.querySelector('#resultado-x1')
+let x2El = document.querySelector('#resultado-x2')
 
 resolverEl.addEventListener('click', resolver);
 
@@ -13,4 +15,14 @@ function resolver() {
   const delta = b ** 2 - 4 * a * c;
 
   deltaEl.value = delta;
+
+  let x1 = null;
+  let x2 = null;
+  if (delta >= 0) {
+    x1 = (-b - Math.sqrt(delta)) / (2*a);
+    x2 = (-b + Math.sqrt(delta)) / (2*a);
+  }
+
+  x1El.value = x1;
+  x2El.value = x2;
 }
